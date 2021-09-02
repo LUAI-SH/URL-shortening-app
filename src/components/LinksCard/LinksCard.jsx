0988072e2cb4c98c4849b3c4d9d8817fdea441dd
Luai <eng.luaish@gmail.com>
2021-09-02 21:59:11 +1000

    Style LinksCard component.

--- src/components/LinksCard/LinksCard.jsx
+++ src/components/LinksCard/LinksCard.jsx
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 
 class LinksCard extends Component {
-  state = {};
+  state = {
+    isClicked: false,
+  };
+
+  handleClicked = () => {
+    this.setState({ isClicked: true });
+    navigator.clipboard.writeText("copied to clip");
+    setInterval(() => this.setState({ isClicked: false }), 5000);
+  };
+
   render() {
     return (
-      <div className="container">
-        <div className="card">
-          <header className="card__header card__link1"></header>
-          <div className="card__body">
-            <span className="card__link2"></span>
-            <button className="btn btn--cyan card__btn">Copy</button>
+      <div className="container center">
+        <div className="card links-card">
+          <header className="links-card__header links-card__link1">
+            someURL
+          </header>
+          <div className="links-card__body">
+            <span className="links-card__link2">someURK</span>
+            <button
+              onClick={this.handleClicked}
+              className={`btn btn--cyan links-card__btn ${
+                this.state.isClicked && "btn--violet btn--violet--animated"
+              }`}
+            >
+              {this.state.isClicked ? "Copied!" : "Copy"}
+            </button>
           </div>
         </div>
       </div>
