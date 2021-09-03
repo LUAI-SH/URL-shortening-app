3acc624a482ba9d8e597a93a86ca4216de6cdebd
Luai <eng.luaish@gmail.com>
2021-09-03 19:27:13 +1000

    Style Hero component.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,15 +3,20 @@ import heroImg from "../../assets/images/illustration-working.svg";
 const Hero = () => {
   return (
     <section className="container hero">
-      <img src={`${heroImg}`} />
-      <div className="hero__img"></div>
+      <div className="" >
+        <img
+          className="hero__img"
+          src={`${heroImg}`}
+          alt="Person working on computer"
+        />
+      </div>
       <div className="hero__content">
-        <h1>More than just shortener links</h1>
-        <p>
+        <h1 className="hero__header">More than just shorter links</h1>
+        <p className="hero__description">
           Build your brand's recognition and get detailed insights on how your
           links are performing.
         </p>
-        <button>Get Started</button>
+        <button className="btn btn--cyan btn--rounded hero__btn">Get Started</button>
       </div>
     </section>
   );
