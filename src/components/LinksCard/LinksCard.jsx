3ab1b0d0feec7b9d4cf3b89f9cb41e225f14ca04
Luai <eng.luaish@gmail.com>
2021-09-03 15:09:37 +1000

    Dynamically generate cards showing shorten link.

--- src/components/LinksCard/LinksCard.jsx
+++ src/components/LinksCard/LinksCard.jsx
@@ -2,34 +2,48 @@ import React, { Component } from "react";
 
 class LinksCard extends Component {
   state = {
-    isClicked: false,
+    buttonsStatus: [],
+    links: [
+      { a: "fgvfdg", b: "dfdsfdf" },
+      { a: "fgvfdg", b: "dfdsfdf" },
+    ],
   };
 
-  handleClicked = () => {
-    this.setState({ isClicked: true });
-    navigator.clipboard.writeText("copied to clip");
-    setInterval(() => this.setState({ isClicked: false }), 5000);
+  handleClicked = async (foo, index) => {
+    foo[index].value = true;
+    await this.setState({ buttonsStatus: foo });
+    console.log(this.state);
+    //navigator.clipboard.writeText(shortenLink);
+    // setInterval(() => this.setState({ buttonsStatus: false }), 5000);
   };
 
   render() {
+    let btnStatus = [];
+    const { links, buttonsStatus } = this.state;
     return (
       <div className="container center">
-        <div className="card links-card">
-          <header className="links-card__header links-card__link1">
-            someURL
-          </header>
-          <div className="links-card__body">
-            <span className="links-card__link2">someURK</span>
-            <button
-              onClick={this.handleClicked}
-              className={`btn btn--cyan links-card__btn ${
-                this.state.isClicked && "btn--violet btn--violet--animated"
-              }`}
-            >
-              {this.state.isClicked ? "Copied!" : "Copy"}
-            </button>
-          </div>
-        </div>
+        {links.map((item, index) => {
+          btnStatus.push({ index, value: false });
+          return (
+            <div key={`${index}`} className="card links-card">
+              <header className="links-card__header links-card__link1">
+                {item.a}
+              </header>
+              <div className="links-card__body">
+                <span className="links-card__link2">{item.b}</span>
+                <button
+                  onClick={() => this.handleClicked(btnStatus, index)}
+                  className={`btn btn--cyan links-card__btn ${
+                    buttonsStatus[index]?.value &&
+                    "btn--violet btn--violet--animated"
+                  }`}
+                >
+                  {buttonsStatus[index]?.value ? "Copied!" : "Copy"}
+                </button>
+              </div>
+            </div>
+          );
+        })}
       </div>
     );
   }
