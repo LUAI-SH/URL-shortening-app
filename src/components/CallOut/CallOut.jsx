a04fcaea86bcbdaa98c19f143ac27cc4608d1c68
Luai <eng.luaish@gmail.com>
2021-09-04 10:09:19 +1000

    Build CallOut component.

---
+++ src/components/CallOut/CallOut.jsx
@@ -0,0 +1,10 @@
+const CallOut = () => {
+  return (
+    <section className="call-out">
+      <h2 className="call-out__heading">Boost your links today</h2>
+      <button className="btn btn--cyan btn--rounded call-out__btn">Get Started</button>
+    </section>
+  );
+};
+
+export default CallOut;
--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ import "./assets/styles/css/style.css";
 import App from "./components/App/App.jsx";
 // import Hero from './components/Hero/Hero.jsx'
 // import Media from "./components/Media/Media.jsx";
-import Statistics from "./components/Statistics/Statistics.jsx";
+// import Statistics from "./components/Statistics/Statistics.jsx";
+import CallOut from "./components/CallOut/CallOut.jsx";
 
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Statistics />, document.getElementById("root"));
+ReactDOM.render(<CallOut />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
