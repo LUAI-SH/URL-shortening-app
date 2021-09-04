a5b75da63c09d5c511f793c310e705122d6420df
Luai <eng.luaish@gmail.com>
2021-09-04 10:31:47 +1000

    Style CallOut component.

--- src/components/CallOut/CallOut.jsx
+++ src/components/CallOut/CallOut.jsx
@@ -2,7 +2,9 @@ const CallOut = () => {
   return (
     <section className="call-out">
       <h2 className="call-out__heading">Boost your links today</h2>
-      <button className="btn btn--cyan btn--rounded call-out__btn">Get Started</button>
+      <button className="btn btn--cyan btn--rounded call-out__btn">
+        Get Started
+      </button>
     </section>
   );
 };
